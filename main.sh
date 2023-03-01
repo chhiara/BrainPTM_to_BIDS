361c230c7c33c17eb03b5cfe3315e85c847c4c42
chhiara <1234chiaa@gmail.com>
2023-03-01 18:54:43 +0100

    added comments!

--- main.sh
+++ main.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+#1. unzip dowloaded folders
 python unzip_folders.py
 
+#2. reoriganize and rename train subjects unzipped data
 python train_subjs_to_BIDS.py
 
+#3.  reoriganize and rename test subjectsunzipped  data
 python train_subjs_to_BIDS.py
+
+#4. create json files to describe the dataset
+
 
 
 
